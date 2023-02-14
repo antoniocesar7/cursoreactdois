a4daed7db41d2fca06dd0237d97a55c7722e5ce9
antoniocesar7 <antoniocesarfernandes@gmail.com>
2023-02-14 15:57:51 -0300

    Aula 30, trabalhando com formulários

---
+++ primeiroapp/src/Appaula29.js
@@ -0,0 +1,31 @@
+// function App() {
+//     return (
+//         <div>
+//             <h1>Bem vindo ao meu projeto</h1>
+//         </div>
+//     );
+// }
+
+// export default App;
+import Nome from './components/Nome';
+import {useState} from 'react';//importanto o nosso hook
+function App(){
+    const[aluno,setAluno] = useState('Sujeito programador')
+
+    function handleChangeName(nome){
+       // alert('TESTE')
+       setAluno(nome);
+    }
+    return(
+        <div>
+            <h1>Componente App</h1><br></br>
+            <h2>Olá: {aluno}</h2>
+            <button onClick={ () => handleChangeName('Asdubal') } >
+                Mudar nome</button><br></br><br></br>
+            {/* <Nome aluno="Antonio" idade={30} /><br/> */}
+                       
+        </div>
+    );
+}
+export default App;
+
